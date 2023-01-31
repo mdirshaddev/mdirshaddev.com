843ab973196d51064a5d49838f0a626f2e85db7b
Md Irshad <mdirshaddev+github@gmail.com>
2023-01-31 10:27:44 +0530

    chore: favicon icon file to this react server component layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout(props: React.PropsWithChildren) {
     <html lang='en'>
       <head>
         <title>Md Irshad - 💫 ServiceNow Developer</title>
+        <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
       </head>
       <body>{props.children}</body>
     </html>
