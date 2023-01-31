f70275cfaf7914870a654f8e0f67971318074d05
Md Irshad <mdirshaddev+github@gmail.com>
2023-01-31 10:15:58 +0530

    config: tailwind and postcss config
    * postcss.config.js - postcss config with tailwindcss
    * tailwind.config.js - tailwind config with next web app content and darkmode with class

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{tsx,ts,jsx,js}', './src/**/*.{tsx,ts,jsx,js}'],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
