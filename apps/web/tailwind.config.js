a332e2d1fabd43a49aad9d2560ce2c5e5a0e24f0
Md Irshad <mdirshaddev+github@gmail.com>
2023-02-02 01:14:01 +0530

    style: extending tailwind color using stylesheet variable

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{tsx,ts,jsx,js}', './src/**/*.{tsx,ts,jsx,js}'],
-  darkMode: 'class',
+  darkMode: ['class', '[data-mode=dark]'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        palette: {
+          primary: 'var(--color-primary)',
+          secondary: 'var(--color-secondary)',
+          success: 'var(--color-success)',
+          info: 'var(--color-info)',
+          warning: 'var(--color-warning)',
+          error: 'var(--color-error)',
+          disabled: 'var(--color-disabled)'
+        }
+      }
+    }
   },
   plugins: []
 };
